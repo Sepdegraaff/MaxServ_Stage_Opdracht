7d3a0811f7dcce306915b4b16550ce07c466f745
Sep de Graaff <sep.degraaff@edu-kw1c.nl>
2023-05-07 21:39:04 +0200

    Making of boxes and styling

--- Website/Index.php
+++ Website/Index.php
@@ -1,1 +1,53 @@
-<?php
+<!doctype html>
+
+<!--
+    Auteur:             Sep de Graaff
+    Aanmaakdatum:       5-7-2023
+
+    Omschrijving:       Main pagina
+ -->
+
+<html>
+    <head>
+        <title>
+            MaxServ Stage Opdracht
+        </title>
+        <!-- Decleratie tekenset -->
+        <meta charset="UTF-8">
+        <!-- Decleratie viewport -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- Link naar css bestand -->
+        <link rel="stylesheet" href="Styles/style.css">
+    </head>
+    <body>
+        <main>
+            <section id="section-wrapper">
+                <div class="row">
+                    <div id="courses-box" class="box-wrapper">
+
+                    </div>
+                    <div class="box-wrapper">
+
+                    </div>
+                    <div class="box-wrapper">
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="box-wrapper">
+
+                    </div>
+                    <div class="box-wrapper">
+
+                    </div>
+                    <div class="box-wrapper">
+
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer>
+
+        </footer>
+    </body>
+</html>
