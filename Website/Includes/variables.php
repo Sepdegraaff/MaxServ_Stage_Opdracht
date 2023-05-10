b7c60601cb50d87a62c314cc7ed67b53447b5cdd
Sepdegraaff <sep.degraaff@edu-kw1c.nl>
2023-05-10 12:41:16 +0200

    finished php and further finalizing (Adding comments).

--- Website/Includes/variables.php
+++ Website/Includes/variables.php
@@ -1,5 +1,107 @@
 <?php
 
+
+    //Link variable to shrink and remove unnecessary code
     $get_started = "<a href='#' class='body-s'>Get started</a>";
+
+
+    //Two variables with all the html code
+    //HTML Structured around 2 rows with different dividers making 6 boxes
+    $row_1 = "<div class='row'>
+                    <div id='courses-box' class='box-wrapper'>
+                        <h1 class='heading-m'>
+                            Check out our most popular courses!
+                        </h1>
+                    </div>
+                    <div class='box-wrapper'>
+                        <div class='icon'>
+                            <img src='Images/Icons/icon-animation.svg' alt='Icon'>
+                        </div>
+                        <h2 class='heading-S'>
+                            Animation
+                        </h2>
+                        <p class='body-m'>
+                            Learn the latest animation
+                            techniques to create stunning
+                            motion design and captivate
+                            your audience.
+                        </p>
+                        
+                        $get_started
+                        
+                    </div>
+                    <div class='box-wrapper'>
+                        <div class='icon'>
+                            <img src='Images/Icons/icon-design.svg' alt='Icon'>
+                        </div>
+                        <h2 class='heading-S'>
+                            Design
+                        </h2>
+                        <p class='body-m'>
+                            Create beautiful, usable
+                            interfaces to help shape the
+                            future of how the web looks.
+                            <br>
+                            <br>
+                        </p>
+                        
+                        $get_started
+                        
+                    </div>
+                </div>";
+
+    $row_2 = "<div class='row'>
+                    <div class='box-wrapper'>
+                        <div class='icon'>
+                            <img src='Images/Icons/icon-photography.svg' alt='Icon'>
+                        </div>
+                        <h2 class='heading-S'>
+                            Photography
+                        </h2>
+                        <p class='body-m'>
+                            Explore critical fundamentals like
+                            lighting, composition, and focus
+                            to capture exceptional photos.
+                        </p>
+                    
+                        $get_started
+                       
+                    </div>
+                    <div class='box-wrapper'>
+                        <div class='icon'>
+                            <img src='Images/Icons/icon-crypto.svg' alt='Icon'>
+                        </div>
+                        <h2 class='heading-S'>
+                            Crypto
+                        </h2>
+                        <p class='body-m'>
+                            All you need to know to get
+                            started investing in crypto. Go
+                            from beginner to advanced with
+                            this 54 hour course.
+                        </p>
+                        
+                        $get_started
+                        
+                    </div>
+                    <div class='box-wrapper'>
+                        <div class='icon'>
+                            <img src='Images/Icons/icon-business.svg' alt='Icon'>
+                        </div>
+                        <h2 class='heading-S'>
+                            Business
+                        </h2>
+                        <p class='body-m'>
+                            A step-by-step playbook to help
+                            you start, scale, and sustain your
+                            business without outside
+                            investment.
+                        </p>
+                        
+                        $get_started
+                        
+                                           
+                    </div>
+                </div>"
 
 ?>
--- Website/Index.php
+++ Website/Index.php
@@ -20,104 +20,21 @@
         <link rel="stylesheet" href="Styles/style.css">
     </head>
     <body>
+        <!--  Main section  -->
         <main>
-            <section id="section-wrapper">
-                <div class="row">
-                    <div id="courses-box" class="box-wrapper">
-                        <h1 class="heading-m">
-                            Check out our most popular courses!
-                        </h1>
-                    </div>
-                    <div class="box-wrapper">
-                        <div class="icon">
-                            <img src="Images/Icons/icon-animation.svg" alt="Icon">
-                        </div>
-                        <h2 class="heading-S">
-                            Animation
-                        </h2>
-                        <p class="body-m">
-                            Learn the latest animation
-                            techniques to create stunning
-                            motion design and captivate
-                            your audience.
-                        </p>
-                        <?php
-                            require 'Includes/variables.php';
+            <?php
+                //Require php file to use the variables
+                require 'Includes/variables.php';
 
-                            echo $get_started;
-                        ?>
-                    </div>
-                    <div class="box-wrapper">
-                        <div class="icon">
-                            <img src="Images/Icons/icon-design.svg" alt="Icon">
-                        </div>
-                        <h2 class="heading-S">
-                            Design
-                        </h2>
-                        <p class="body-m">
-                            Create beautiful, usable
-                            interfaces to help shape the
-                            future of how the web looks.
-                            <br>
-                            <br>
-                        </p>
-                        <?php
-                            echo $get_started;
-                        ?>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="box-wrapper">
-                        <div class="icon">
-                            <img src="Images/Icons/icon-photography.svg" alt="Icon">
-                        </div>
-                        <h2 class="heading-S">
-                            Photography
-                        </h2>
-                        <p class="body-m">
-                            Explore critical fundamentals like
-                            lighting, composition, and focus
-                            to capture exceptional photos.
-                        </p>
-                        <?php
-                            echo $get_started;
-                        ?>
-                    </div>
-                    <div class="box-wrapper">
-                        <div class="icon">
-                            <img src="Images/Icons/icon-crypto.svg" alt="Icon">
-                        </div>
-                        <h2 class="heading-S">
-                            Crypto
-                        </h2>
-                        <p class="body-m">
-                            All you need to know to get
-                            started investing in crypto. Go
-                            from beginner to advanced with
-                            this 54 hour course.
-                        </p>
-                        <?php
-                            echo $get_started;
-                        ?>
-                    </div>
-                    <div class="box-wrapper">
-                        <div class="icon">
-                            <img src="Images/Icons/icon-business.svg" alt="Icon">
-                        </div>
-                        <h2 class="heading-S">
-                            Business
-                        </h2>
-                        <p class="body-m">
-                            A step-by-step playbook to help
-                            you start, scale, and sustain your
-                            business without outside
-                            investment.
-                        </p>
-                        <?php
-                            echo $get_started;
-                        ?>
-                    </div>
-                </div>
+            ?>
+            <!--Section wrapper to wrap the two rows-->
+            <section id="section-wrapper">
+                <!--Short code echoing the 2 variables filled with HTML-->
+                <?php
+                    echo $row_1;
+
+                    echo $row_2;
+                ?>
             </section>
         </main>
         <footer>
